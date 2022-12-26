416a4d5c9944e407e2cadcbc5713f8ee5f51152d
fahd labba <labbafahd012@gmail.com>
2022-12-26 02:05:26 +0100

    fix some bug

--- Controller/mail_sender.js
+++ Controller/mail_sender.js
@@ -1,27 +1,27 @@
-const nodemailer=require('nodemailer')
+// const nodemailer=require('nodemailer')
 
-const sendmail=(name,mail)=> {
-    let transporter=nodemailer.createTransport({
-        service:'gmail',
-        auth:{
-            user:'testapinode12@gmail.com',
-            pass:'kfbrayzzxoijtqrn'
-        }
-    })
-    let mailOptions = {
-        from: 'testapinode12@gmail.com',
-        to: mail,
-        subject: 'Verification',
-        text: 'Welcome '+name+' to our application \n to complete ur registration please click sur le lien suivant\n Developer : Fahd Labba ',
-    }
-    transporter.sendMail(mailOptions,(error,info)=>{
-        if(error){
-            console.log(error)
-        }else{
-            console.log('Email sent : '+info.response)
-        }
-    })
-}
+// const sendmail=(name,mail)=> {
+//     let transporter=nodemailer.createTransport({
+//         service:'gmail',
+//         auth:{
+//             user:'testapinode12@gmail.com',
+//             pass:'kfbrayzzxoijtqrn'
+//         }
+//     })
+//     let mailOptions = {
+//         from: 'testapinode12@gmail.com',
+//         to: mail,
+//         subject: 'Verification',
+//         text: 'Welcome '+name+' to our application \n to complete ur registration please click sur le lien suivant\n Developer : Fahd Labba ',
+//     }
+//     transporter.sendMail(mailOptions,(error,info)=>{
+//         if(error){
+//             console.log(error)
+//         }else{
+//             console.log('Email sent : '+info.response)
+//         }
+//     })
+// }
 
 
-module.exports=sendmail
+// module.exports=sendmail

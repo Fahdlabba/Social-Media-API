6e4747d943aba7a6db31b57c3f214a1aca92d960
fahd labba <labbafahd012@gmail.com>
2023-01-16 09:14:09 +0100

    fix url path

--- startup/startup.js
+++ startup/startup.js
@@ -14,19 +14,19 @@ const application=(app)=>{
     app.set('view engine', 'html');
     app.set('views', __dirname);
     //User 
-    app.post('/login',login)
-    app.post('/create',create)
-    app.get('/forget_pass',GetPassword)
-    app.post('/forget_pass',PostPassword)
-    app.put('/verification',VerifyUser)
-    app.delete('/delete',DeleteUser)
+    app.post('/user/login',login)
+    app.post('/user/create',create)
+    app.get('/user/forget_pass',GetPassword)
+    app.post('/user/forget_pass',PostPassword)
+    app.put('/user/verification',VerifyUser)
+    app.delete('/user/delete',DeleteUser)
     //User Post
-    app.post('/add_post',AddPost)
-    app.post('/like',LikePost)
-    app.post('/unlike',UnlikePost)
-    app.delete('/delete_post',DeletePost)
+    app.post('/user/post/add_post',AddPost)
+    app.post('/user/post/like',LikePost)
+    app.post('/user/post/unlike',UnlikePost)
+    app.delete('/user/post/delete_post',DeletePost)
     //User Messages 
-    app.post('/SendMessage',SendMessage)
-    app.get('/UserMesaage',UserMessages)
+    app.post('/user/message/SendMessage',SendMessage)
+    app.get('/user/message/UserMesaage',UserMessages)
 }
 module.exports=application
